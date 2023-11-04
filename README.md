359b22f4507b43fb0fe9c7b90fd9fb64487853a2
CRaez-Dev <craez.dev@gmail.com>
2023-11-04 00:37:34 -0500

    Adding clone flow

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # solid-principles
+
 Repository to index each SOLID principles's project made in different environments.
+
+### Get Proyects
+
+`npm run clone`
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { existsSync, mkdirSync, readdirSync } from 'node:fs';
+import { simpleGit, SimpleGit } from 'simple-git'
+import { PROJECTS } from "./constants";
+
+const BASE_URL: string = 'https://github.com/CRaez-Dev/';
+const APP_DIR: string = './apps';
+
+(async (): Promise<void> => {
+    try {
+        console.log("🚀 Checking app's folders")
+        if (!existsSync(APP_DIR)) mkdirSync(APP_DIR)
+        const git: SimpleGit = simpleGit();
+        const REPO_FOLDERS = readdirSync(APP_DIR)
+
+        console.log("🚀 Cloning process -> started")
+        await Promise.all(PROJECTS.map(async (item) => {
+            if (!REPO_FOLDERS.some((repo) => repo === item)) {
+                console.log(`🚀 Cloning -> ${item}`)
+                await git.clone(`${BASE_URL}${item}`, `./apps/${item}`)
+                console.log('🚀 Cloning -> succeded')
+            }
+        }))
+        console.log("🚀 Cloning process -> ended")
+
+    } catch (error) {
+        console.log('🚀 Error on cloning process', error)
+    }
+})()
